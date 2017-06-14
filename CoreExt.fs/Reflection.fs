8534a0f73893fcdc4391d680cc24859b31297e08
BenBeattieHood <benb@pageuppeople.com>
2017-06-14 10:13:27 +1000

    Added lenses by statically resolved type parameters (draft, not used by quotations yet)

---
+++ CoreExt.fs/Reflection.fs
@@ -0,0 +1,28 @@
+﻿namespace FSharp.CoreExt.Reflection
+
+module ReflectionUtils =
+    let recordTypeWith<'T> (mapWith: (string * obj) -> obj) (o:'T): 'T =
+        let oType = typeof<'T>
+        let ctorParamValues =
+            oType.GetProperties()
+            |> Array.map
+                (fun propertyInfo ->
+                    sprintf "%c%s" (System.Char.ToLowerInvariant propertyInfo.Name.[0]) (propertyInfo.Name.Substring 1),
+                    propertyInfo.GetValue o
+                )
+            |> Array.map
+                (fun kv ->
+                    fst kv, 
+                    mapWith kv
+                )
+            |> Map.ofArray
+        let ctor = o.GetType().GetConstructors().[0]
+    
+        ctor.GetParameters()
+        |> Array.map
+            (fun parameterInfo ->
+                ctorParamValues
+                |> Map.find parameterInfo.Name
+            )
+        |> ctor.Invoke
+        |> (fun x -> x :?> 'T)
--- Optics/Sandbox.fs
+++ Optics/Sandbox.fs
@@ -1,18 +1,101 @@
 ﻿module Sandbox
 
-open Optics
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open System.IO
+open System.Reflection
 
 
-[<Lens>]
-type Address = {
-    street1: string
-    street2: string
-    city: string
-}
 
-[<Lens>]
-type Person = {
+//let compileToDynamic 
+//    (s: string)
+//    : Assembly =
+//    use reader = System.IO.string
+//    let chk = FSharpChecker.Create()
+//    async {
+//        let! asd = chk.ParseAndCheckProject ()
+//        asd.
+//    }
+//    let compileErrors, compileExitCode, assembly =
+//        chk.CompileToDynamicAssembly(
+//            [| 
+//                "-o"
+//                null
+//                "-a"
+
+//            |],
+//            execute = None
+//            )
+//    assembly
+
+let inline FirstName_< ^a when ^a : (member firstName : string) > () =
+    (fun o -> (^a : (member firstName : string) o)),
+    (fun (value: string) (o: ^a) ->
+        o |> FSharp.CoreExt.Reflection.ReflectionUtils.recordTypeWith 
+            (fun (k, v) ->
+                match k,v with
+                | "firstName", _ -> upcast value
+                | _, value -> value
+            )
+    ),
+    (fun (value: string) (o: ^a) ->
+        { o with
+            ^a.firstName = value
+        }
+    )
+
+type Lenses = Optics.LenProvider<"c:\dev\myproj\debug\bin\myproj.dll">
+let firstNameGetter, firstNameSetter = Lenses.FirstName_()
+
+let get = fst
+let set = snd
+
+    
+type Bear = {
     firstName: string
-    lastName: string
-    address: Address
 }
+let pete:Bear = {
+    firstName = "Pete"
+}
+
+type Person = {
+    firstName: int
+}
+
+let david:Person = {
+    firstName = 123
+}
+
+let davidsName = FirstName_() |> get <| david 
+
+let petesName = FirstName_() |> get <| pete
+
+
+
+//open Microsoft.FSharp.Reflection
+ 
+//let (?) x m args =
+//    let args =
+//        if box args = null then (* the unit value is null, once boxed *)
+//            [||]
+//        elif FSharpType.IsTuple (args.GetType()) then
+//            FSharpValue.GetTupleFields args
+//        else
+//            [|args|]
+//    let result = x.GetType().InvokeMember(m, System.Reflection.BindingFlags.GetProperty ||| System.Reflection.BindingFlags.InvokeMethod, null, x, args)
+//    args |> Array.head |> (fun x -> x.ToString())
+
+
+
+
+//type Bear = {
+//    firstName: int
+//}
+//let pete:Bear = {
+//    firstName = 123
+//}
+
+//let asd2 = getFirstName_ pete
+
+//let asd = setFirstName_ "asd" pete 
+
+//let petesName = pete ? first3Name()
